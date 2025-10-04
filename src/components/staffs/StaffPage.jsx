20dacb73a1a3a4c1ea8b9564a84454db0d97a861
AspiringSaint <wilberttosoydev@outlook.com>
2025-10-04 12:23:19 +0800

    implemented the tabs component in staff page

--- src/components/staffs/StaffPage.jsx
+++ src/components/staffs/StaffPage.jsx
@@ -1,29 +1,20 @@
 import { NavLink, Outlet } from 'react-router-dom';
+
+import Tabs from '../common/Tabs';
 import '../../styles/components/staffs/StaffPage.css';
 
 const StaffPage = () => {
+
+    const tabItems = [
+        { to: '/admin/staffs', label: 'Staff List', end: true },
+        { to: '/admin/staffs/register', label: 'Register Staff' }
+    ]
+
     return (
         <div className="staff-page">
             <h1>Staff Management</h1>
 
-            {/* Tabs navigation */}
-            <nav className="staff-tabs">
-                <NavLink
-                    to="/admin/staffs"
-                    end
-                    className={({ isActive }) => `staff-tab ${isActive ? "active" : ""}`}
-                >
-                    Staff List
-                </NavLink>
-
-                <NavLink
-                    to="/admin/staffs/register"
-                    className={({ isActive }) => `staff-tab ${isActive ? "active" : ""}`}
-                >
-                    Register Staff
-                </NavLink>
-            </nav>
-
+            <Tabs tabs={tabItems} />
             {/* Render nested route here */}
             <div className="staff-outlet">
                 <Outlet />
